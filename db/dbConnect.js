521a79bf9ce7adae492b05402067b1c389f8f32a
tarun <tarunkumar8059@gmail.com>
2023-09-27 02:45:54 -0700

    set env variabels

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require('sequelize');
-
-
-  const sequelize=new Sequelize('test1', 'root', '1234', {
-  host: 'localhost',
+require("dotenv").config();
+const dbName=process.env.DB_Name
+const user=process.env.User
+const password=process.env.Password
+const host=process.env.HOST
+  const sequelize=new Sequelize(dbName, user, password, {
+  host: host,
   dialect: 'mysql'
 })
 
