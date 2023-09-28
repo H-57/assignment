8097daec969bf5b3db955b0ad1a7cb853112cf0d
tarun <tarunkumar8059@gmail.com>
2023-09-27 23:41:05 -0700

    fix db uri

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,20 +1,14 @@
 const { Sequelize } = require('sequelize');
 require("dotenv").config();
-const dbName=process.env.DB_Name||process.env.POSTGRES_DATABASE
-const user=process.env.User||process.env.POSTGRES_USER
-const password=process.env.Password||process.env.POSTGRES_PASSWORD
-const host=process.env.HOST||process.env.POSTGRES_HOST
-  const sequelize=new Sequelize(dbName, user, password, {
-  host: host,
-  dialectModule: require('pg'),
-  dialect: 'postgres',
-  
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialectModule: require('pg'), 
   ssl: true, // Enable SSL
   dialectOptions: {
     ssl: {
       require: true, // Use sslmode=require
     },}
-})
+});
 
 
 module.exports=sequelize
